58b84eae65afa387ec4d427dbae9f662abeb1556
Isaac <bonaventuraisaac52@gmail.com>
2025-07-17 22:46:40 +0700

    Test minimal home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,5 @@ import Banner from './components/Banner';
 import PostList from './components/PostList';
 
 export default function Home() {
-  return (
-    <main>
-      <Banner />
-      <section className="max-w-5xl mx-auto px-4 py-8">
-        <PostList />
-      </section>
-    </main>
-  );
+  return <div>Test Hello World</div>;
 }
