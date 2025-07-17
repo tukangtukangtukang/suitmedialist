327e2dc6ed84d7d9d4ce7ad23537450f3b9581c4
Isaac <bonaventuraisaac52@gmail.com>
2025-07-17 23:21:07 +0700

    Refactor Home component to include Banner and PostList within a structured layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,12 @@ import Banner from './components/Banner';
 import PostList from './components/PostList';
 
 export default function Home() {
-  return <div>Test Hello World</div>;
+  return (
+    <main>
+      <Banner />
+      <section className="max-w-5xl mx-auto px-4 py-8">
+        <PostList />
+      </section>
+    </main>
+  );
 }
